e0ea336436745d009d4f7e300571065b83223d0c
Geiko <k.i.geiko@gmail.com>
2017-03-17 02:08:02 +0200

    ae

--- js/linq_db_0.js
+++ js/linq_db_0.js
@@ -1,16 +1,15 @@
-'use strict'
-	  
+'use strict'	  
 var rate_0 = 
 [
 	{	
-		q : "Выбрть из массива строки, начинающиеся на букву \"F\" и отсортировать полученный список.<br /><br />\\n"+
-			"List<string> list = new List<string>();<br />\\n"+
+		q : "Выбрать из массива строки, начинающиеся на букву \"F\" и отсортировать полученный список.<br /><br />\n"+
+			"List<string> list = new List<string>();<br />\n"+
             "list.AddRange(new string[] { \"Asya\", \"Fedya\", \"Nikolay\", \"Anna\", \"Fan\" });", 			
 		
 		a : "var x = from n in list<br />\n"+
-			"		 where n.StartsWith(\"F\")\n"+
-			"		 orderby n\n"+
-			"		 select n;\n\n"+
+			"\twhere n.StartsWith(\"F\")\n"+
+			"\torderby n\n"+
+			"\tselect n;\n\n"+
 			"var x = list.Where(n => n.StartsWith(\"F\")).OrderBy(n => n);"
 	},
 	{	
