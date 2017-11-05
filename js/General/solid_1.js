f18d7a6532f91a2bf17c4608416163597b1552d2
geiko <k.i.geiko@gmail.com>
2017-11-05 12:04:11 +0200

    solid

--- js/General/solid_1.js
+++ js/General/solid_1.js
@@ -10,31 +10,43 @@ var solid_1 =
 		task : "1. What is SOLID?", 			
 		
 		solution : "SOLID - это пять основных принципов объектно-ориентированного программирования и проектирования.\n\n" +
-		"S\tSRP (The Single Responsibility Principle)\n" +
-		"\tПринцип единственной ответственности.\n" +
-		"\tСуществует лишь одна причина, приводящая к появлению класса.\n\n" +
 
-		"O\tOCP (The Open Closed Principle)\n" +
-		"\tПринцип открытости/закрытости.\n" +
-		"\t«программные сущности … должны быть открыты для расширения,\n" +
-		"\tно закрыты для модификации.»\n\n" +
+		"S\t The Single Responsibility \n" +
+		"\t A class should have only a single responsibility \n" +
+		"\t (i.e. changes to only one part of the software's specification \n" +
+		"\t should be able to affect the specification of the class) \n\n" +
+		"\t Принцип единственной ответственности.\n" +
+		"\t Существует лишь одна причина, приводящая к появлению класса.\n\n" +
+
+		"O\t The Open Closed \n" +
+		"\t “software entities … should be open for extension,\n" +
+		"\t but closed for modification.”\n\n" +
+		"\t Принцип открытости/закрытости.\n" +
+		"\t «программные сущности … должны быть открыты для расширения,\n" +
+		"\t но закрыты для модификации.»\n\n" +
 		
-		"L\tLSP (The Liskov Substitution Principle)\n" +  
-		"\tПринцип подстановки Барбары Лисков.\n" +
-		"\t«объекты в программе должны быть заменяемыми\n" + 
-		"\tна экземпляры их подтипов\n" +
-		"\tбез изменения правильности выполнения программы.»\n\n" +
+		"L\t The Liskov Substitution \n" +  
+		"\t “Objects in a program should be replaceable with \n" +
+		"\t instances of their subtypes without \n" +
+		"\t altering the correctness of that program.”\n\n" +
+		"\t Принцип подстановки Барбары Лисков.\n" +
+		"\t «объекты в программе должны быть заменяемыми\n" + 
+		"\t на экземпляры их подтипов\n" +
+		"\t без изменения правильности выполнения программы.»\n\n" +
 
-		"I\tISP (The Interface Segregation Principle)\n" +
-		"\tПринцип разделения интерфейса.\n" +
-		"\t«много интерфейсов,\n" + 
-		"\tспециально предназначенных для клиентов, лучше,\n" +
-		"\tчем один интерфейс общего назначения.»\n\n" +
+		"I\t The Interface Segregation \n" +
+		"\t “Many client-specific interfaces are better \n" +
+		"\t than one general-purpose interface.”\n\n" +
+		"\t Принцип разделения интерфейса.\n" +
+		"\t «много интерфейсов,\n" + 
+		"\t специально предназначенных для клиентов, лучше,\n" +
+		"\t чем один интерфейс общего назначения.»\n\n" +
 
-		"D\tDIP (The Dependency Inversion Principle)\n" +
-		"\tПринцип инверсии зависимостей.\n" +
-		"\t«Зависимость на Абстракциях.\n" +
-		"\tНет зависимости на что-то конкретное.»"
+		"D\t The Dependency Inversion \n" +
+		"\t One should “depend upon abstractions, not concretions.”\n\n" +
+		"\t Принцип инверсии зависимостей.\n" +
+		"\t «Зависимость на Абстракциях.\n" +
+		"\t Нет зависимости на что-то конкретное.»"
 	}
 
 	// {
